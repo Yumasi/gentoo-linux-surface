bbdbd1ecf2bcc7cf3596710ff03bfc83fb1d1329
Guillaume Pagnoux <guillaume.pagnoux@epita.fr>
2019-08-11 15:19:23 +0200

    Add install script
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@epita.fr>

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # gentoo-linux-surface
-A kernel and firmware patcher for Surface devices
+
+A kernel and firmware patcher for Surface devices. This patches
+`sys-kernel/gentoo-sources` with patches from
+[Jakeday's repository](https://github.com/jakeday/linux-surface). It is meant to
+be used with *stable* `gentoo-sources`. This should probably work with any
+distribution of the kernel you may use on your Gentoo install; just modify the
+install script to insert the patches in the proper place in
+`/etc/portage/patches/`.
+
+Again, I am **not** the original author of the patches, these are the result of
+the awesome work by Jakeday. Any request and discussion about having Linux
+working on Surface devices should go
+[here](https://github.com/jakeday/linux-surface).
+
+## Kernel version
+
+This install patches for the `4.19` version of the kernel. Tested on: `4.19.57`.
+
+## Installation
+
+``` shell
+git clone https://github.com/Yumasi/gentoo-linux-surface.git && cd gentoo-linux-surface
+./setup.sh
+```
+
+This script must be run as `root` to copy patches in the portage directory, to
+rebuild the kernel, and install it. It does not support building an initramfs at
+the moment.
+
+As any scripts that runs `root` on your machine, you *should* read it and
+understand what it does. Don't blindly trust me! (Even though in this case you
+can.) Also, it uses verbose options as much as possible so you know what it
+does.
+
+## TODO
+
+- Better version checks
+- Firmware installation
+- Initramfs generation
---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Options
+NO_BUILD=0
+NO_EMERGE=0
+WITH_CONFIG=0
+
+# Config
+CONFIG_FILE='config-4.19.57-gentoo'
+KERNEL_NAME='sys-kernel/gentoo-sources'
+KERNEL_PATH='/usr/src/linux'
+MAKEFLAGS='-j5'
+PATCHES_DESTINATION="/etc/portage/patches/$KERNEL_NAME"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+print_help() {
+    echo 'Available options:'
+    echo '--with-config: install provided configuration. Off by default.'
+    echo '--no-emerge: skip installation of patches and emerging of kernel'
+    echo '             sources. Off by default.'
+    echo '--no-build: skip kernel building. Off by default.'
+    echo '-h: displays this help page.'
+}
+
+while [ "$#" -ne 0 ]; do
+    case "$1" in
+         '--with-config')
+             WITH_CONFIG=1
+             ;;
+         '--no-emerge')
+             NO_EMERGE=1
+             ;;
+         '--no-build')
+             NO_BUILD=1
+             ;;
+         '-h')
+             print_help
+             exit 0
+             ;;
+        *)
+          echo "Unrecognized option: $1"
+          exit 2
+    esac
+
+    shift
+done
+
+if [ "$NO_EMERGE" -eq 0 ]; then
+    mkdir -pv "$PATCHES_DESTINATION"
+    cp -v "$PWD"/patches/* "$PATCHES_DESTINATION"
+    emerge -av "$KERNEL_NAME"
+fi
+
+if [ "$WITH_CONFIG" -ne 0 ]; then
+    cp -v "$PWD"/configs/"$CONFIG_FILE" "$KERNEL_PATH"/.config
+    make -C "$KERNEL_PATH" olddefconfig
+fi
+
+if [ "$NO_BUILD" -eq 0 ]; then
+    make -C "$KERNEL_PATH" "$MAKEFLAGS"
+    make -C "$KERNEL_PATH" modules_install
+    make -C "$KERNEL_PATH" install
+fi
